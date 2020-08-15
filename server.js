f2bcef65755395030d8f96f9fd6628470879795b
applegrubber <jscottatwood@gmail.com>
2020-08-14 23:38:25 -0600

    Added inline logic

--- server.js
+++ server.js
@@ -6,37 +6,64 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./View/Sytles")))
 app.use(express.json());
 
-var reservations = [
-    {
-        name: "Example",
-        phone: "555-1234",
-        email: "abc@123.com",
-        id: "4567"
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./View/default.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all characters
+app.get("/api/characters", function(req, res) {
+  return res.json(characters);
+});
+
+// Displays a single character, or returns false
+app.get("/api/characters/:character", function(req, res) {
+  var chosen = req.params.character;
+
+  console.log(chosen);
+
+  for (var i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
     }
-]
+  }
 
-/**
- * Routes
- */
+  return res.json(false);
+});
 
- app.get("/", function(req, res){
-     res.sendFile(path.join(__dirname, "index.html"));
- });
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newcharacter = req.body;
 
- app.get("/tables", function(req, res){
-    res.sendfile(path.join(__dirname, "viewTable.html"));
- });
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
- app.get("/reservation", function(req, res){
-    res.sendfile(path.join(__dirname, "reservation.html"));
- });
+  console.log(newcharacter);
 
- app.post("/add-table", function(req, res) {
+  characters.push(newcharacter);
 
- })
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
